2bd45e8ea154e6f5f6e6f6862f5927f330372d40
vlad <vladislav.rublevskiy@gmail.com>
2019-04-27 22:42:51 +0300

    Evaluate index expressions.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -71,6 +71,16 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		}
 
 		return applyFunction(function, args)
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	case *ast.ArrayLiteral:
 		el := evalExpressions(node.Elements, env)
 		if len(el) == 1 && isError(el[0]) {
@@ -153,6 +163,27 @@ func evalInfixExpression(operator string, left, right object.Object) object.Obje
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NIL
+	}
+
+	return arrayObject.Elements[idx]
 }
 
 func evalBangOperatorExpression(right object.Object) object.Object {
