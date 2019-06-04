e8e00f4a1173a6a730d0b44c5f0cb37fe815de22
vlad <vladislav.rublevskiy@gmail.com>
2019-06-04 22:38:59 +0300

    Add conveyor.

--- repl/repl.go
+++ repl/repl.go
@@ -2,49 +2,80 @@ package repl
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
+	"github.com/user/courseWork/ast"
 	"github.com/user/courseWork/evaluator"
 	"github.com/user/courseWork/lexer"
 	"github.com/user/courseWork/object"
 	"github.com/user/courseWork/parser"
 	"io"
+	"sync"
 )
 
 const PROMT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
-	scanner := bufio.NewScanner(in)
-	env := object.NewEnvironment()
 
-	for {
-		fmt.Printf(PROMT)
-		scanned := scanner.Scan()
+	wg := new(sync.WaitGroup)
+	wg.Add(3)
 
-		if !scanned {
-			return
+	analysis := make(chan string)
+	evaluation := make(chan *ast.Program)
+	printing := make(chan string)
+
+	go func() {
+		defer wg.Done()
+		scanner := bufio.NewScanner(in)
+		for {
+			fmt.Print(PROMT)
+			scanned := scanner.Scan()
+			if !scanned {
+				close(analysis)
+				return
+			}
+			line := scanner.Text()
+			analysis <- line
+			ostr := <-printing
+			if ostr != "" {
+				io.WriteString(out, ostr)
+				io.WriteString(out, "\n")
+			}
 		}
+	}()
 
-		line := scanner.Text()
-		l := lexer.New(line)
-		p := parser.New(l)
-
-		program := p.ParseProgram()
-
-		if len(p.Errors()) != 0 {
-			printParseErrors(out, p.Errors())
-			continue
+	go func() {
+		defer wg.Done()
+		for line := range analysis {
+			l := lexer.New(line)
+			p := parser.New(l)
+			program := p.ParseProgram()
+			if len(p.Errors()) != 0 {
+				var err bytes.Buffer
+				for _, msg := range p.Errors() {
+					err.WriteString("\t" + msg + "\n")
+				}
+				printing <- err.String()
+				continue
+			}
+			evaluation <- program
 		}
+		close(evaluation)
+	}()
 
-		evaluated := evaluator.Eval(program, env)
-		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+	go func() {
+		defer wg.Done()
+		env := object.NewEnvironment()
+		for program := range evaluation {
+			evaluated := evaluator.Eval(program, env)
+			if evaluated != nil {
+				printing <- evaluated.Inspect()
+			} else {
+				printing <- ""
+			}
 		}
-	}
-}
+		close(printing)
+	}()
 
-func printParseErrors(out io.Writer, errors []string) {
-	for _, msg := range errors {
-		io.WriteString(out, "\t"+msg+"\n")
-	}
+	wg.Wait()
 }
